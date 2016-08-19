2dbc011a20d86284faca5b90964e1b737d49f2bb
annie <annie55880000@gmail.com>
2016-08-18 18:52:46 -0700

    5.10 admin role

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,11 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def destroy
+  @comment = Comment.find(params[:id])
+  product = @comment.product
+  @comment.destroy
+  redirect_to product
+  end
+
 end
