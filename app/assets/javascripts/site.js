7138be35b01edefac6426c44244de2b29acae930
annie <annie55880000@gmail.com>
2016-09-07 18:16:09 -0700

    JavaScript

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,10 @@
-$(document).on('ready page:load', function(){
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-
+$(document).on('turbolinks:load', function(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+  $(".img-zoom").elevateZoom ( {width: 350});
 });
